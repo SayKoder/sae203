48bd7f766133d891534555bac7f1990afd95811b
SayKoder <prenom-nom@etudiant.univ-reims.fr>
2023-03-22 16:28:03 +0000

    Test Table

--- sql/sae203Base.sql
+++ sql/sae203Base.sql
@@ -16,59 +16,61 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Createur`
+-- Table structure for table `createur`
 --
 
-DROP TABLE IF EXISTS `Createur`;
+DROP TABLE IF EXISTS `createur`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Createur` (
+CREATE TABLE `createur` (
   `crea_id` int(11) NOT NULL AUTO_INCREMENT,
   `crea_nom` char(25) NOT NULL COMMENT 'Nom des créateurs des ARGs et mythes',
   `crea_plateforme` char(25) NOT NULL COMMENT 'Plateforme utilisée lors de la création de l''ARG ou mythe',
   `crea_nationalitee` char(2) NOT NULL COMMENT 'Nationalitée du créateur',
   PRIMARY KEY (`crea_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Createur`
+-- Dumping data for table `createur`
 --
 
-LOCK TABLES `Createur` WRITE;
-/*!40000 ALTER TABLE `Createur` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Createur` ENABLE KEYS */;
+LOCK TABLES `createur` WRITE;
+/*!40000 ALTER TABLE `createur` DISABLE KEYS */;
+INSERT INTO `createur` VALUES (1,'Anonyme','Inconnue','EN'),(2,'Alan Resnick','Youtube','US'),(3,'Michael P. Crowe','Blogs','US'),(4,'Troy Wagner et Tim Sutton','Youtube','US'),(5,'Eric Knudsen','Youtube','EN'),(6,'Sam Gorski','Blogs','US'),(7,'Troy Hasbrouck','Youtube','EN'),(8,'Matt Dymerski','Youtube','EN'),(9,' Brian Brushwood','Blogs','US'),(10,'Anonyme','Youtube','EN'),(11,'Troy Wagner','Blogs','EN'),(12,'Kris Straub','Blogs','US'),(13,'Tony Domenico','Blogs','US'),(14,'Adam Rosner','Youtube','US'),(15,'Vincent Cuseo','Fandom','EN'),(16,'Michael Divinski','Twitter','US'),(17,'Carrie Zylka','Youtube','EN'),(18,'Nick Kelly','Youtube','US'),(19,'Troy Hasbrouck','Twitter','EN'),(20,'Alan Resnick','Youtube','US'),(21,'Zack Akers','Film','EN'),(22,'Joseph DeLage','Film','EN'),(23,'Fans de Half-Life','Fandom','US'),(24,'Communeautee SCP','Blogs','X'),(25,'Jesse Petersen','Litterature','EN'),(26,' Kelly Andrew','Litterature','US'),(27,'Lewis Packwood','Blogs','US'),(28,' Eric Heisserer','Blogs','EN'),(29,'Alex Hall','Youtube','US'),(30,'42 Entertainement','Blogs','US'),(31,'Funcom','Blogs','US'),(32,'Drew Sykes','Film','EN'),(33,'Andrew Miller','Film','US'),(34,'Con Artist Games','Blogs','US'),(35,'Sarah Schachner','Youtube','US'),(36,'Nine Inch Nails','Blogs','EN'),(37,'Bill Gallagher','Film','US'),(38,'Anonyme','Blogs','EN'),(39,'Thomas Hercouët','Twitter','FR'),(40,'42 Entertainement','Blogs','US'),(41,'X','Blogs','EN'),(42,'Jesse Petersen','Blogs','US'),(43,'Brian Brushwood','Blogs','EN'),(44,'42 Entertainement','Blogs','US'),(45,'IAM','Youtube','US'),(46,'Alice & Smith','Jeux-Vidéo','EN'),(47,'Mind Candy','Blogs','US'),(48,'X','Blogs','US'),(49,'X','Film','US'),(50,'Communeautee BKR','Blogs','US');
+/*!40000 ALTER TABLE `createur` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `Mythes`
+-- Table structure for table `mythes`
 --
 
-DROP TABLE IF EXISTS `Mythes`;
+DROP TABLE IF EXISTS `mythes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Mythes` (
+CREATE TABLE `mythes` (
   `mARG_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Identifiant des mythes et ARGs',
   `mARG_titre` char(45) NOT NULL COMMENT 'Titres des ARGs et mythes',
   `mARG_annee` year(4) NOT NULL COMMENT 'Année d''apparition/création des ARGs et mythes',
-  `mARG_photo` varchar(30) NOT NULL COMMENT 'Photo représentant les ARGs et mythes',
-  `mARG_resume` blob NOT NULL COMMENT 'Description/resumes des ARGs et mythes',
+  `mARG_desc` varchar(255) NOT NULL COMMENT 'Description et resumes ',
+  `mARG_photo` varchar(50) DEFAULT NULL,
   `mARG_type` char(25) NOT NULL COMMENT 'Types de l''histoire (entre ARG ou mythe d''internet : tréfond, début de l''iceberg ...)',
   `mARG_popularitee` char(15) NOT NULL COMMENT 'Montrer son influence/popularitee sur le web (Faible/FOrt/Trés fort/Traumatisant...)',
   `_crea_id` int(11) NOT NULL COMMENT 'Identifiant créateurs ARGs et mythes (clé étrangère)',
   PRIMARY KEY (`mARG_id`),
   KEY `crea_id` (`_crea_id`),
-  CONSTRAINT `crea_id` FOREIGN KEY (`_crea_id`) REFERENCES `Createur` (`crea_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  CONSTRAINT `crea_id` FOREIGN KEY (`_crea_id`) REFERENCES `createur` (`crea_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `Mythes`
+-- Dumping data for table `mythes`
 --
 
-LOCK TABLES `Mythes` WRITE;
-/*!40000 ALTER TABLE `Mythes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Mythes` ENABLE KEYS */;
+LOCK TABLES `mythes` WRITE;
+/*!40000 ALTER TABLE `mythes` DISABLE KEYS */;
+INSERT INTO `mythes` VALUES (2,'alantutorial',2011,'un homme qui crée des tutoriels étranges et déroutants. Les vidéos contiennent des éléments perturbants et une histoire complexe qui se déroule au fil du temps.','alantutorial.jpg','ARG','Connu',2),(3,'The Wests Records',2013,'The West Records suit l\'histoire d\'un groupe de scientifiques qui tentent de comprendre les mystères d\'une ville abandonnée. Ils découvrent bientôt qu\'ils sont en danger et qu\'ils doivent trouver un moyen de s\'en sortir.','thewestrecords.jpg','Mythe','Connu',3),(4,'Clear Lakes 44 / Slender-man',2014,'Clear Lakes 44 suit l\'histoire d\'un homme qui commence à recevoir des appels étranges d\'une entreprise mystérieuse. Il découvre bientôt qu\'il est suivi par une présence inquiétante, le Slender Man.','clearlake44.png','Mythe','Trés Connu',4),(8,'fdg',2013,'Bonjour','2023_03_21_13_53_34---Chaise.jpg','ml','sef',1);
+/*!40000 ALTER TABLE `mythes` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -80,4 +82,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-03-02  8:53:45
+-- Dump completed on 2023-03-22 16:16:58
